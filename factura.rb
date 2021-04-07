45fc96cc41fb0db1d3f482ff482e6275da9df0f4
Marcos Gutierrez <marcos.gutierrez@gazmows.com>
2021-04-06 22:01:15 -0400

    5- Imprimir total sumando impuesto CA (MVP)

--- factura.rb
+++ factura.rb
@@ -2,7 +2,9 @@ cantidad =ARGV[0].to_i
 precio_unitario = ARGV[1].to_i
 
 neto = cantidad * precio_unitario
+impuesto = neto * 0.0825
+total = neto + impuesto
 
 puts "#{cantidad} x $#{precio_unitario} = $#{neto}"
-puts "CA(%0) = $0"
-puts "Total = $#{neto}"
+puts "CA(%8.25) = $#{impuesto}"
+puts "Total = $#{total}"
