9e8bee366ddeb27abb1d4f5257f0364d8ee06c01
Marcos Gutierrez <marcos.gutierrez@gazmows.com>
2021-04-06 22:21:06 -0400

    7- Imprimir total aplicando descuento

--- factura.rb
+++ factura.rb
@@ -6,14 +6,21 @@ class Factura
     attr_accessor :cantidad, :precio_unitario
     def facturar()
         neto = self.cantidad * self.precio_unitario
-        impuesto = neto * 0.0825
-        total = neto + impuesto
+        impuesto = neto * 0.0825        
+        total_parcial = neto + impuesto
+        descuento = calcular_descuento total_parcial
+        total = neto + impuesto - descuento
         
         puts "#{self.cantidad} x $#{self.precio_unitario} = $#{neto}"
         puts "CA(%8.25) = $#{impuesto}"
-        puts "DTO(%0) = $0"
+        puts "DTO(%0) = $#{descuento}"
         puts "Total = $#{total}"
     end
+
+    def calcular_descuento(total)
+        return 0
+    end
+    private :calcular_descuento
 end
 
 cantidad = ARGV[0].to_i
