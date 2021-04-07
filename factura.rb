11f0ffd65d3fa3077d00d2b7fdd630a3cce3390d
Marcos Gutierrez <marcos.gutierrez@gazmows.com>
2021-04-06 22:14:30 -0400

    6- Imprimir detalle de descuento aplicando 0%

--- factura.rb
+++ factura.rb
@@ -1,10 +1,23 @@
-cantidad =ARGV[0].to_i
+class Factura
+    def initialize(cantidad, precio_unitario)
+        self.cantidad = cantidad
+        self.precio_unitario = precio_unitario
+    end
+    attr_accessor :cantidad, :precio_unitario
+    def facturar()
+        neto = self.cantidad * self.precio_unitario
+        impuesto = neto * 0.0825
+        total = neto + impuesto
+        
+        puts "#{self.cantidad} x $#{self.precio_unitario} = $#{neto}"
+        puts "CA(%8.25) = $#{impuesto}"
+        puts "DTO(%0) = $0"
+        puts "Total = $#{total}"
+    end
+end
+
+cantidad = ARGV[0].to_i
 precio_unitario = ARGV[1].to_i
 
-neto = cantidad * precio_unitario
-impuesto = neto * 0.0825
-total = neto + impuesto
-
-puts "#{cantidad} x $#{precio_unitario} = $#{neto}"
-puts "CA(%8.25) = $#{impuesto}"
-puts "Total = $#{total}"
+factura = Factura.new cantidad, precio_unitario
+factura.facturar
