d35dfc00e9204592f13b4e4ab372809edff57a4b
Marcos Gutierrez <marcos.gutierrez@gazmows.com>
2021-04-06 22:50:10 -0400

    13- Aplicar impuesto de UT

--- factura.rb
+++ factura.rb
@@ -1,26 +1,33 @@
 class Factura
-    def initialize(cantidad, precio_unitario)
+    def initialize(cantidad, precio_unitario, estado)
         self.cantidad = cantidad
         self.precio_unitario = precio_unitario
+        self.estado = estado
     end
-    attr_accessor :cantidad, :precio_unitario
+    attr_accessor :cantidad, :precio_unitario, :estado
     def facturar()
         neto = self.cantidad * self.precio_unitario
-        impuesto = neto * 0.0825        
-        total_parcial = neto + impuesto
+
+        tasa_impuesto = calcular_impuesto self.estado
+        total_impuesto = neto * tasa_impuesto/100
+        
+        total_parcial = neto + total_impuesto
+        
         descuento = calcular_descuento total_parcial
+        
         total_descuento = total_parcial * descuento/100
 
         total = total_parcial - total_descuento/100
         
         puts "#{self.cantidad} x $#{self.precio_unitario} = $#{neto}"
-        puts "CA(%8.25) = $#{impuesto}"
+        puts "#{estado}(%#{tasa_impuesto}) = $#{total_impuesto}"
         puts "DTO(%#{descuento}) = $#{total_descuento}"
         puts "Total = $#{total}"
     end
 
+    private
     def calcular_descuento(total)
-        total_descuento = 0;
+        total_descuento = 0
         if total > 50000
             total_descuento =  15
         elsif total > 10000
@@ -35,11 +42,23 @@ class Factura
 
         return total_descuento
     end
-    private :calcular_descuento
+
+    def calcular_impuesto(estado)
+        total_impuesto = 0
+
+        if estado == "CA"
+            total_impuesto = 8.25
+        elsif estado == "UT"
+            total_impuesto = 6.85
+        end
+        
+        return total_impuesto
+    end
 end
 
 cantidad = ARGV[0].to_i
 precio_unitario = ARGV[1].to_i
+estado = ARGV[2]
 
-factura = Factura.new cantidad, precio_unitario
+factura = Factura.new cantidad, precio_unitario, estado
 factura.facturar
